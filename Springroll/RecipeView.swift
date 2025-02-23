144112b2002ac0e23f15cc3ed02abc9ee64b9269
myon-mcgee <myonmcgee1@gmail.com>
2025-02-23 15:11:06 -0600

    created Recipe View outline. Next is to combine pages

--- Springroll/RecipeView.swift
+++ Springroll/RecipeView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct RecipeView: View {
+    
+    //@State private var ingredients = []
+    //@State private var instructions = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Text("Springroll")
+                .font(.largeTitle)
+                .bold()
+            
+            HStack{
+                Spacer()
+                Button {
+                    print("Button Tapped")
+                } label: {
+                    Image(systemName: "ellipsis")
+                        .font(.title3)
+                        .foregroundColor(Color.black)
+                }.padding(.trailing)
+            }.padding(.bottom)
+            
+            NavigationView {
+                List {
+                    Section(header: Text("Ingredients").font(.title2)) {
+                        /*ForEach(fruits, id: \.self) { item in
+                            
+                        }*/
+                    }
+                    
+                    Section(header: Text("Instructions").font(.title2)){
+                        /*ForEach(fruits, id: \.self) { item in
+                            
+                        }*/
+                    }
+                }.listStyle(.plain)
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            VStack {
+                                Text("Recipe Name")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Text("Time")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+            }
+            Spacer()
+        }
     }
 }
 
